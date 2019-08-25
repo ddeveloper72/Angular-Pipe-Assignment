1791b34cb532ef923814c0416f0a14bb9d076d83
ddeveloper72 <duncan.niamh@gmail.com>
2019-08-25 01:12:49 +0100

    Reverse the status text.

--- src/app/reverse.pipe.ts
+++ src/app/reverse.pipe.ts
@@ -1,12 +1,9 @@
 import { Pipe, PipeTransform } from '@angular/core';
-
 @Pipe({
   name: 'reverse'
 })
 export class ReversePipe implements PipeTransform {
-
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(value: any): any {
+    return value.split('').reverse().join('');
   }
-
 }
